e8f5601661fdf148a9429deca0154bbcdae1c690
extreeme <azvini@outlook.com>
2023-03-01 22:26:27 -0300

    second challenge

--- classes/class03/pages/video-player/src/index.js
+++ classes/class03/pages/video-player/src/index.js
@@ -1,3 +1,5 @@
+import handGestureFactory from "../../titles/src/factories/handGestureFactory.js"
 import factory from "./factory.js"
 
 await factory.initialize()
+await handGestureFactory.initialize()
