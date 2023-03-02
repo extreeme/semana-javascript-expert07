1c7448c316e416dc1152d172d8e1a282d2f2ac39
extreeme <azvini@outlook.com>
2023-03-02 16:20:47 -0300

    class04

---
+++ classes/class04/pages/video-player/src/index.js
@@ -0,0 +1,5 @@
+import handGestureFactory from "../../titles/src/factories/handGestureFactory.js"
+import factory from "./factory.js"
+
+await factory.initialize()
+await handGestureFactory.initialize()
